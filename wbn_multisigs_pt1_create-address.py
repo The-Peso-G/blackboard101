39ec92df9079f27cfc56e9650d4cc8319e9ab63f
johnsondiao <diaochengdong@gmail.com>
2014-05-21 21:37:45 +0800

    modify for chinese
    use the newest interface and modify the function for useing not
    education

--- wbn_multisigs_pt1_create-address.py
+++ wbn_multisigs_pt1_create-address.py
@@ -5,53 +5,28 @@
 # We had to change the bitcoinrpc 'connection.py' file to add multisig support
 # https://github.com/wobine/blackboard101/blob/master/wbn_multisigs_pt1_create-address.py
 
-from bitcoinrpc.util import *
-from bitcoinrpc.exceptions import *
-from bitcoinrpc.__init__ import *
-from bitcoinrpc.config import *
-from bitcoinrpc.proxy import *
-from bitcoinrpc.data import *
-from bitcoinrpc.connection import *
 
+from bitcoinrpc.authproxy import AuthServiceProxy
 
-bitcoin = connect_to_local() #creates an object called 'bitcoin' that allows for bitcoind calls
+bitcoin = AuthServiceProxy("http://test:123456@127.0.0.1:8332") #creates an object called 'bitcoin' that allows for bitcoind calls
 
-add = dict()
-privkey = dict()
 pubkey = dict()
-mid = "\",\"" #this thing inserts these 3 characters ","
 
 
-for i in range(0, 3): #Generate three new addresses (Pub Key & Priv Key)
-    print
-    print "Brand New Address Pair: Number", i+1
-    
-    add[i] = bitcoin.getnewaddress()
-    print "Compressed Public Address -",len(add[i]),"chars -", add[i]
-    
-    privkey[i] = bitcoin.dumpprivkey(add[i])
-    print "Private Key -", len(privkey[i]),"chars -",privkey[i]
-
-    validDate = bitcoin.validateaddress(add[i]) # we need a less compressed Public Key so we have to call validateaddress to find it
-    pubkey[i] = validDate["pubkey"]
-    print "Less compressed Public Key/Address -",len(pubkey[i]),"chars -",pubkey[i]
-
-print
-print "For fun you can paste this into bitcoind to verify multisig address"
-print "%s%s%s%s%s%s%s" % ('bitcoind createmultisig 2 \'["',pubkey[0],mid,pubkey[1],mid,pubkey[2],'"]\'')
-
-print
+print "请输入公钥，公钥可以由命令[validate address]的输出pubKey字段得到"
+pubkey[0] = str(raw_input("请输入管理者1的公钥："))
+pubkey[1] = str(raw_input("请输入管理者2的公钥："))
+pubkey[2] = str(raw_input("请输入管理者3的公钥："))
+n = int(raw_input("请输入几个私钥可以解锁："))
 threeaddy = [pubkey[0],pubkey[1],pubkey[2]]
-print "The multisig address is"
-multisigaddy = bitcoin.addmultisigaddress(2,threeaddy)
-multiaddyandredeem = (bitcoin.createmultisig(2,threeaddy))
+print "多重签名地址是："
+multisigaddy = bitcoin.addmultisigaddress(n,threeaddy)
+multiaddyandredeem = (bitcoin.createmultisig(n,threeaddy))
 print len(multisigaddy),"chars - ", multisigaddy
 print
-print "The redeemScript -", len(multiaddyandredeem["redeemScript"]), "chars -",multiaddyandredeem["redeemScript"]
+print "redeemScript -", len(multiaddyandredeem["redeemScript"]), "chars -",multiaddyandredeem["redeemScript"]
 print
-print "Now copy all this output text and save it so you'll be ready for part two."
-print "Also, you can send a tiny amt of bitcoins to this multisig address",multisigaddy,"to fund it,"
-print "Next time we'll go through all the steps to spend from your new multisig address."
+print "现在可以把上面的输出都存起来，方便以后花费时构建输出单"
 
 
 
--- wbn_multisigs_pt2_spend-funds.py
+++ wbn_multisigs_pt2_spend-funds.py
@@ -6,130 +6,116 @@
 # We had to change the bitcoinrpc 'connection.py' file to add multisig support
 # you'll need to download our 'connection.py' file from Github & stuff it in your bitcoinrpc folder
 
-from bitcoinrpc.util import *
-from bitcoinrpc.exceptions import *
-from bitcoinrpc.__init__ import *
-from bitcoinrpc.config import *
-from bitcoinrpc.proxy import *
-from bitcoinrpc.data import *
-from bitcoinrpc.connection import *
+#import sys;
+#sys.path.append("D:\github\python-bitcoinrpc\bitcoinrpc")
+
+from bitcoinrpc.authproxy import AuthServiceProxy
 
 
-bitcoin = connect_to_local() #creates an object called 'bitcoin' that allows for bitcoind calls
 
-# YOU NEED AT LEAST TWO OF THE PRIVATE KEYS FROM PART ONE linked to your MULTI-SIG ADDRESS
-multisigprivkeyone = "L2M1uRgdwgCotoP8prWJYYwz2zwWgsMa9TJwqARG7nFxkpdvBSsm" #your key/brother one
-multisigprivkeytwo = "L1M2ZgjoAtDVu9uemahiZBQPwFA5Tyj4GLd1ECkDryviFrGp6m7k" #wallet service/brother two
-multisigprivkeythree = "L5PkVBzR4SdQimMsfHnRqRegJZDFJ22sGjSbfp3SsPSnVoB8vRFE" #safe deposit box/brother three
-ChangeAddress = "35Z3xG92YkW5Xo4ngQw6w5b3Ce6MDw94A8" #!!! Makes Sure to set your own personal Change Address
+bitcoin = AuthServiceProxy("http://test:123456@127.0.0.1:8332")#creates an object called 'bitcoin' that allows for bitcoind calls
 
 SetTxFee = int(0.00005461*100000000) # Lets proper good etiquette & put something aside for our friends the miners
 
+ChangeAddress = bitcoin.getnewaddress();
+
 unspent = bitcoin.listunspent() # Query wallet.dat file for unspent funds to see if we have multisigs to spend from
 
-print "Your Bitcoin-QT/d has",len(unspent),"unspent outputs"
+print "你的钱包里这些地址有 ",len(unspent)," 个地址可以花费"
 for i in range(0, len(unspent)):
     print
-    print "Output",i+1,"has",unspent[i]["amount"],"bitcoins, or",int(unspent[i]["amount"]*100000000),"satoshis"
-    print "The transaction id for output",i+1,"is"
+    print "第 ",i+1," 个地址有 ",unspent[i]["amount"]," 个比特币，相当于 ",int(unspent[i]["amount"]*100000000),"聪"
+    print "它的传输ID ",i+1,"是"
     print unspent[i]["txid"]
-    print "The ScriptPubKey is", unspent[i]["scriptPubKey"]
-    print "on Public Address =====>>",unspent[i]["address"]
+    print "ScriptPubKey： ", unspent[i]["scriptPubKey"]
+    print "地址 =====>>",unspent[i]["address"]
 
 print
 totalcoin = int(bitcoin.getbalance()*100000000)
-print "The total value of unspent satoshis is", totalcoin
+print "钱包总额是：", totalcoin, "聪"
 print
 
-WhichTrans = int(raw_input('Spend from which output? '))-1
+WhichTrans = int(raw_input('你想花费哪个地址上的币? '))-1
 if WhichTrans > len(unspent): #Basic idiot check. Clearly a real wallet would do more checks.
-    print "Sorry that's not a valid output" 
+    print "抱歉这个地址不存在，请确认您输入的序号" 
 else:
     tempaddy = str(unspent[WhichTrans]["address"])
     print
     if int(tempaddy[0:1]) == 1:
-        print "The public address on that account starts with a '1' - its not multisig."
+        print "这是一个普通地址，请输入多重签名地址的序号"
     elif int(tempaddy[0:1]) == 3:
-        print "The public address on that account is",tempaddy
-        print "The address starts with the number '3' which makes it a multisig."
+        print "地址是",tempaddy
+        print "以3开头的地址意味着这是一个多重签名地址."
         print
-        print "All multisig transactions need: txid, scriptPubKey and redeemScript"
-        print "Fortunately all of this is right there in the bitcoind 'listunspent' json from before"
+        print "发送多重签名地址上的币，需要如下的一些参数: txid, scriptPubKey,  redeemScript"
+        print "这些命令可以在listunspent命令里获得"
         print
-        print "The txid is:",unspent[WhichTrans]["txid"]
-        print "The ScriptPubKey is:", unspent[WhichTrans]["scriptPubKey"]
+        print "txid :",unspent[WhichTrans]["txid"]
+        print "ScriptPubKey :", unspent[WhichTrans]["scriptPubKey"]
         print
-        print "And only multisigs have redeemScripts."
-        print "The redeemScript is:",unspent[WhichTrans]["redeemScript"]
+        print "redeemScript :",unspent[WhichTrans]["redeemScript"]
         print
         
-        print "You have",int(unspent[WhichTrans]["amount"]*100000000),"satoshis in this output."
+        print "在这个地址里有 ",int(unspent[WhichTrans]["amount"]*100000000)," 聪"
 
-        HowMuch = int(raw_input('How much do you want to spend? '))
+        HowMuch = int(raw_input('您想支出多少 '))
         if HowMuch > int(unspent[WhichTrans]["amount"]*100000000):
-            print "Sorry not enough funds in that account" # check to see if there are enough funds.
+            print "抱歉账户里没有这么多钱" # check to see if there are enough funds.
         else:
             print
-            SendAddress = str(raw_input('Send funds to which bitcoin address? ')) or "1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd" #default value Sean's Outpost
-            if SendAddress == "1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd":
-                print "Nice! Your chose to send funds to Sean's Outpost in Pensacola Florida."
+            
+            SendAddress = str(raw_input('发送到哪个地址？ (例如33hxAeUqNnFs3gdayu7aAaijhTbbfnphq8) ')) 
+            if SendAddress == "33hxAeUqNnFs3gdayu7aAaijhTbbfnphq8":
+                print "太感谢了，你选择捐赠给johnson Diao"
             print
             Leftover = int(unspent[WhichTrans]["amount"]*100000000)-HowMuch-SetTxFee
-            print "This send to",SendAddress,"will leave", Leftover,"Satoshis in your accounts."
-            print "A tx fee of",SetTxFee,"will be sent to the miners"
+            print "将要发送您的比特币到这个地址：",SendAddress,"账户里会留下来 ", Leftover," 聪","，这些币会发送到找零地址：",ChangeAddress
+            print "会发送 ",SetTxFee," 聪的网络手续费给矿工"
             print
-            print "Creating the raw transaction for User One - Private Key One"
-            print
+            print "将要创建发送单"
+            
             
             rawtransact = bitcoin.createrawtransaction ([{"txid":unspent[WhichTrans]["txid"],
                     "vout":unspent[WhichTrans]["vout"],
                     "scriptPubKey":unspent[WhichTrans]["scriptPubKey"],
                     "redeemScript":unspent[WhichTrans]["redeemScript"]}],{SendAddress:HowMuch/100000000.00,ChangeAddress:Leftover/100000000.00})
-            print "bitcoind decoderawtransaction", rawtransact
+            print "发送单为：", rawtransact
             print
             print
-            print "And now we'll sign the raw transaction -> The first user gets a 'False'"
-            print "This makes sense because in multisig, no single entity can sign alone"
-            print
-            print "For fun you can paste this FIRST signrawtransaction into bitcoind to verify multisig address"
-            print "%s%s%s%s%s%s%s%s%s%s%s%s%s" % ('bitcoind signrawtransaction \'',rawtransact,'\' \'[{"txid":"',unspent[WhichTrans]["txid"],'","vout":',
-                                      unspent[WhichTrans]["vout"],',"scriptPubKey":"',unspent[WhichTrans]["scriptPubKey"],'","redeemScript":"',
-                                      unspent[WhichTrans]["redeemScript"],'"}]\' \'["',multisigprivkeyone,'"]\'')
+            print "现在我们要用私钥进行签名"
+            multisigprivkeyone = str(raw_input("请输入第一个私钥："))
             print
             signedone = bitcoin.signrawtransaction (rawtransact,
                     [{"txid":unspent[WhichTrans]["txid"],
                     "vout":unspent[WhichTrans]["vout"],"scriptPubKey":unspent[WhichTrans]["scriptPubKey"],
                     "redeemScript":unspent[WhichTrans]["redeemScript"]}],
                     [multisigprivkeyone])
+            print "签名结果"
             print signedone
             print
-            print "In a real world situation, the 'hex' part of this thing above would be sent to the second"
-            print "user or the wallet provider. Notice, the private key is not there. It has been signed digitally"
+            print "在实际应用时，您可以把上面的数据发给第二个密钥的持有者，由他完成第二步签名，上述信息不会泄露您的私钥，因为"
+            print "私钥已经经过了加密"
             print
-            print
-            print "For fun you can paste this SECOND signrawtransaction into bitcoind to verify multisig address"
-            print "%s%s%s%s%s%s%s%s%s%s%s%s%s" % ('bitcoind signrawtransaction \'',signedone["hex"],'\' \'[{"txid":"',unspent[WhichTrans]["txid"],'","vout":',
-                                      unspent[WhichTrans]["vout"],',"scriptPubKey":"',unspent[WhichTrans]["scriptPubKey"],'","redeemScript":"',
-                                      unspent[WhichTrans]["redeemScript"],'"}]\' \'["',multisigprivkeytwo,'"]\'')
+            multisigprivkeytwo = str(raw_input("请输入第二个私钥："))
             print
             doublesignedrawtransaction = bitcoin.signrawtransaction (signedone["hex"],
                     [{"txid":unspent[WhichTrans]["txid"],
                     "vout":unspent[WhichTrans]["vout"],"scriptPubKey":unspent[WhichTrans]["scriptPubKey"],
                     "redeemScript":unspent[WhichTrans]["redeemScript"]}],
                     [multisigprivkeytwo])
+            print "第二步签名的结果是"
             print doublesignedrawtransaction
             print
-            print "You are now ready to send",HowMuch,"Satoshis to",SendAddress
-            print "And",Leftover,"Satoshis will be sent to the change account",ChangeAddress
-            print "Finally, a miner's fee of ",SetTxFee,"Satoshis will be sent to the miners"
+            print "现在您已经准备好发送",HowMuch,"聪的比特币到",SendAddress
+            print Leftover," 聪的比特币会发送到找零地址：",ChangeAddress
+            print "网络手续费是 ",SetTxFee," 聪"
             print
 
-            ReallyNow = (raw_input('If you hit return now, you will be sending these funds from your multisig account '))
-            ReallyNow2 = (raw_input('No...REally...If you hit return now, you will be sending funds from your multisig account '))
+            ReallyNow = (raw_input('如果现在点击回车，这笔钱将从多重地址发送出去，您确定吗？'))
+            ReallyNow2 = (raw_input('真的？您真的打算发送这笔钱？ '))
             print
-            print "SORRY. We won't do this. Don't want anyone to lose money playing with this code"
-            print "But if you really want to send it, just"
-            print "copy the HEX from the big block up above"
-            print "and put it in a 'bitcoind sendrawtransaction' request"
+            print "哈哈，我们不会把钱发送出去的。我们可不希望您因为运行脚本受到任何损失"
+            print "如果您真的想发送这笔钱的话，可以将下面这一坨东西复制到bitcoin-qt的命令窗口，然后回车，币就真的发出去了"
+            print "sendrawtransaction "+"\""+str(doublesignedrawtransaction['hex'])+"\""
 
         
